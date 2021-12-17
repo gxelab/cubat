5379085c4d2326f9d975aea70098fab12344abc5
Hebin-zhang <misakaheb@163.com>
2021-12-18 03:49:03 +0800

    Statistical codon frequency

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import re
-# import biopython
+
+import pandas as pd
 from Bio.Seq import Seq
 import numpy as np
 import csv
-import linecache
+
+
+def get_data(data_path, line_begin, line_end):
+    # Extract sequences and merge into one line string
+    import linecache
+    lines = linecache.getlines(data_path)[line_begin:line_end]  # Extract sequences
+    lines = [line.strip() for line in lines]  # Remove '\n'
+    lines = ''.join(lines)  # Merge into one line string
+    return lines
 
 
 def cut(obj, sec):
@@ -11,11 +20,33 @@ def cut(obj, sec):
     return [obj[i:i + sec] for i in range(0, len(obj), sec)]
 
 
-testData = linecache.getline('Test_SRA/SRR17184641.fastq', 2)  # Extract a sequence from SRR17184641
-DNA_seq = Seq(testData)
-mRNA_seq = DNA_seq.reverse_complement().transcribe()  # Transcribed into mRNA
-startCodon = 'AUG.*'
-jumpStartingPoint = re.search(startCodon, str(mRNA_seq))  # Jump to start point
-StartingPoint = jumpStartingPoint.group(0)
-result = cut(str(StartingPoint), 3)  # Cutting RNA sequence
+def locate_start_codon(selected_start_codon, mRNA_seq):
+    start_codon = selected_start_codon + '.*'
+    jump_to_starting_point = re.search(start_codon, str(mRNA_seq))  # Jump to start point
+    starting_point = jump_to_starting_point.group(0)
+    return starting_point
+
+
+def locate_stop_codon(cut_seq):
+    if cut_seq.count('UGA'):
+        return cut_seq.index('UGA')
+    elif cut_seq.count('UAA'):
+        return cut_seq.index('UAA')
+    elif cut_seq.count('UAG'):
+        return cut_seq.index('UAG')
+
+
+def process_mRNA_seq(location_of_stop_codon):
+    return cut_seq[0:location_of_stop_codon]
+
+
+Sars_cov_2 = get_data('Test_Data/Sars_cov_2.ASM985889v3.cds.fasta', 1, 356)  # Extract a sequence
+DNA_seq = Seq(Sars_cov_2)
+mRNA_seq = DNA_seq.transcribe()  # Transcribed into mRNA
+located_seq = locate_start_codon('AUG', mRNA_seq)
+cut_seq = cut(located_seq, 3)  # Cutting RNA sequence
+location_of_stop_codon = locate_stop_codon(cut_seq)
+processed_mRNA_seq = process_mRNA_seq(location_of_stop_codon)
+result = pd.value_counts(processed_mRNA_seq)
 print(result)
+`
