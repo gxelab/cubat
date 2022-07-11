7ae12775aaab963b943a9956cd2c6d4f28897cb6
Misakaheb <80016897+Hebin-zhang@users.noreply.github.com>
2022-07-11 16:47:31 +0800

    Rewrite RSCU

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import re
+import time
+import numpy as np
 import pandas as pd
 from Bio import SeqIO
+# from Bio.Seq import Seq
+from Bio.Data import CodonTable
 
 
 def generate_info_dataframe(parse_data):
@@ -24,45 +29,101 @@ def generate_info_dataframe(parse_data):
     return info_dataframe
 
 
+def count_codon(seq_list):
+    codon_num = {'TTT': 0, 'TTC': 0, 'TTA': 0, 'TTG': 0, 'TCT': 0, 'TCC': 0, 'TCA': 0, 'TCG': 0, 'TAT': 0, 'TAC': 0,
+                 'TAA': 0, 'TAG': 0, 'TGT': 0,
+                 'TGC': 0, 'TGA': 0, 'TGG': 0, 'CTT': 0, 'CTC': 0, 'CTA': 0, 'CTG': 0, 'CCT': 0, 'CCC': 0, 'CCA': 0,
+                 'CCG': 0,
+                 'CAT': 0,
+                 'CAC': 0, 'CAA': 0, 'CAG': 0, 'CGT': 0, 'CGC': 0, 'CGA': 0, 'CGG': 0, 'ATT': 0, 'ATC': 0, 'ATA': 0,
+                 'ATG': 0,
+                 'ACT': 0, 'ACC': 0, 'ACA': 0, 'ACG': 0, 'AAT': 0, 'AAC': 0, 'AAA': 0, 'AAG': 0, 'AGT': 0, 'AGC': 0,
+                 'AGA': 0,
+                 'AGG': 0, 'GTT': 0, 'GTC': 0, 'GTA': 0, 'GTG': 0, 'GCT': 0, 'GCC': 0, 'GCA': 0, 'GCG': 0, 'GAT': 0,
+                 'GAC': 0,
+                 'GAA': 0, 'GAG': 0, 'GGT': 0, 'GGC': 0, 'GGA': 0, 'GGG': 0}
+
+    depart = len(seq_list) / 8
+    times = int(depart)
+    rest = int((depart - times) * 8)
+    for i in range(times):
+        codon_num[seq_list.pop()] += 1
+        codon_num[seq_list.pop()] += 1
+        codon_num[seq_list.pop()] += 1
+        codon_num[seq_list.pop()] += 1
+        codon_num[seq_list.pop()] += 1
+        codon_num[seq_list.pop()] += 1
+        codon_num[seq_list.pop()] += 1
+    for i in range(rest):
+        codon_num[seq_list.pop()] += 1
+
+    codon_array = np.array(list(codon_num.values()))
+
+    return codon_array
+
+
 def generate_codon_dataframe(parse_data):
-    codon_dataframe = pd.DataFrame(
-        columns=["TTT", "TTC", "TTA", "TTG",
-                 "TCT", "TCC", "TCA", "TCG",
-                 "TAT", "TAC", "TAA", "TAG",
-                 "TGT", "TGC", "TGA", "TGG",
-                 "CTT", "CTC", "CTA", "CTG",
-                 "CCT", "CCC", "CCA", "CCG",
-                 "CAT", "CAC", "CAA", "CAG",
-                 "CGT", "CGC", "CGA", "CGG",
-                 "ATT", "ATC", "ATA", "ATG",
-                 "ACT", "ACC", "ACA", "ACG",
-                 "AAT", "AAC", "AAA", "AAG",
-                 "AGT", "AGC", "AGA", "AGG",
-                 "GTT", "GTC", "GTA", "GTG",
-                 "GCT", "GCC", "GCA", "GCG",
-                 "GAT", "GAC", "GAA", "GAG",
-                 "GGT", "GGC", "GGA", "GGG"])
-    for record in parse_data:
-        codon_dataframe.loc[record.id] = [0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0,
-                                          0, 0, 0, 0]
-        for codon in [str(record.seq)[i:i + 3] for i in range(0, len(record.seq), 3)]:
-            codon_dataframe.loc[record.id, codon == codon_dataframe.columns] += 1
+    parse_data = list(parse_data)
+    times = len(parse_data)
+    result_array = np.zeros((times, 64), dtype=int)
+    extracted_name = []
+    for t in range(0, times):
+        extracted_name.append(parse_data[t].name)
+        seq_str = str(parse_data[t].seq)
+        seq_list = re.findall('.{3}', seq_str)
+        result_array[t] = count_codon(seq_list)
+    codon_dataframe = pd.DataFrame(result_array,
+                                   columns=['TTT', 'TTC', 'TTA', 'TTG',
+                                            'TCT', 'TCC', 'TCA', 'TCG',
+                                            'TAT', 'TAC', 'TAA', 'TAG',
+                                            'TGT', 'TGC', 'TGA', 'TGG',
+                                            'CTT', 'CTC', 'CTA', 'CTG',
+                                            'CCT', 'CCC', 'CCA', 'CCG',
+                                            'CAT', 'CAC', 'CAA', 'CAG',
+                                            'CGT', 'CGC', 'CGA', 'CGG',
+                                            'ATT', 'ATC', 'ATA', 'ATG',
+                                            'ACT', 'ACC', 'ACA', 'ACG',
+                                            'AAT', 'AAC', 'AAA', 'AAG',
+                                            'AGT', 'AGC', 'AGA', 'AGG',
+                                            'GTT', 'GTC', 'GTA', 'GTG',
+                                            'GCT', 'GCC', 'GCA', 'GCG',
+                                            'GAT', 'GAC', 'GAA', 'GAG',
+                                            'GGT', 'GGC', 'GGA', 'GGG'],
+                                   index=extracted_name)
+
     return codon_dataframe
 
 
-Sars2 = SeqIO.parse("Sars_cov_2.ASM985889v3.cds.fasta", "fasta")
-print(generate_info_dataframe(Sars2))
+def rscu(codon_dataframe):
+    stop_codon = []
+    rscu_dataframe = pd.DataFrame(columns=codon_dataframe.columns, index=codon_dataframe.index)
+    for codon in codon_dataframe.columns:
+        try:
+            for seq in codon_dataframe.index:
+                # /
+                amino_acid_value = CodonTable.standard_dna_table.forward_table[codon]
+                rscu_amino_acid = \
+                    [key for key, value in CodonTable.standard_dna_table.forward_table.items() if
+                     value == amino_acid_value]
+                codon_frequency = 0
+                for rscu_codon in rscu_amino_acid:
+                    codon_frequency += codon_dataframe.at[seq, rscu_codon]
+                    if codon_frequency / len(rscu_amino_acid):
+                        rscu_dataframe.at[seq, codon] = \
+                            codon_dataframe.at[seq, codon] / (codon_frequency / len(rscu_amino_acid))
+
+        except KeyError:
+            stop_codon.append(codon)
+    return rscu_dataframe
+
+
+start = time.time()
+
+Sars2 = SeqIO.parse("Test_Data/Sars_cov_2.ASM985889v3.cds.fasta", "fasta")
+Sars2_codon_dataframe = generate_codon_dataframe(Sars2)
+print(rscu(Sars2_codon_dataframe))
+
+
+# print(Sars2_info_dataframe)
+# print(Sars2_codon_dataframe)
+print(time.time() - start)
