f66eeca0ebcff52c2a26663ff67b47231d37b0f4
lzyy <95127165+lzyy@users.noreply.github.com>
2022-08-02 15:46:52 +0800

    Update genecode_data.py

--- genecode_data.py
+++ genecode_data.py
@@ -1,4 +1,7 @@
 import numpy as np
+from Bio.Data import CodonTable
+import pandas as pd
+
 
 class genecode_compute():
     stop_codon_location = None
@@ -15,36 +18,37 @@ class genecode_compute():
     all_amifamilies = None
 
     @staticmethod
-    def _compute_genecode(amino_acid_sequence,
-                          TTT=None,
-                          TTC=None, TTA=None, TTG=None, TCT=None, TCC=None, TCA=None, TCG=None, TAT=None, TAC=None,
-                          TAA=None,
-                          TAG=None, TGT=None, TGC=None, TGA=None, TGG=None, CTT=None, CTC=None, CTA=None, CTG=None,
-                          CCT=None,
-                          CCC=None, CCA=None, CCG=None, CAT=None, CAC=None, CAA=None, CAG=None, CGT=None, CGC=None,
-                          CGA=None,
-                          CGG=None, ATT=None, ATC=None, ATA=None, ATG=None, ACT=None, ACC=None, ACA=None, ACG=None,
-                          AAT=None,
-                          AAC=None, AAA=None, AAG=None, AGT=None, AGC=None, AGA=None, AGG=None, GTT=None, GTC=None,
-                          GTA=None,
-                          GTG=None, GCT=None, GCC=None, GCA=None, GCG=None, GAT=None, GAC=None, GAA=None, GAG=None,
-                          GGT=None,
-                          GGC=None, GGA=None, GGG=None):
-        amino_acid_arr = np.array(list(amino_acid_sequence))
-        order = np.array(
-            [TTT, TTC, TTA, TTG, TCT, TCC, TCA, TCG, TAT, TAC, TAA, TAG, TGT, TGC, TGA, TGG, CTT, CTC, CTA, CTG, CCT,
-             CCC,
-             CCA, CCG, CAT, CAC, CAA, CAG, CGT, CGC, CGA, CGG, ATT, ATC, ATA, ATG, ACT, ACC, ACA, ACG, AAT, AAC, AAA,
-             AAG,
-             AGT, AGC, AGA, AGG, GTT, GTC, GTA, GTG, GCT, GCC, GCA, GCG, GAT, GAC, GAA, GAG, GGT, GGC, GGA, GGG])
-        changed_location = np.nonzero(order)
-        if len(changed_location) > 0:
-            amino_acid_arr[changed_location] = order[order != None]
-
+    def get_genecode(genecode):
+        codon_table = CodonTable.unambiguous_dna_by_id[genecode].forward_table
+        codons = ["TTT", "TTC", "TTA", "TTG",
+                  "TCT", "TCC", "TCA", "TCG",
+                  "TAT", "TAC", "TAA", "TAG",
+                  "TGT", "TGC", "TGA", "TGG",
+                  "CTT", "CTC", "CTA", "CTG",
+                  "CCT", "CCC", "CCA", "CCG",
+                  "CAT", "CAC", "CAA", "CAG",
+                  "CGT", "CGC", "CGA", "CGG",
+                  "ATT", "ATC", "ATA", "ATG",
+                  "ACT", "ACC", "ACA", "ACG",
+                  "AAT", "AAC", "AAA", "AAG",
+                  "AGT", "AGC", "AGA", "AGG",
+                  "GTT", "GTC", "GTA", "GTG",
+                  "GCT", "GCC", "GCA", "GCG",
+                  "GAT", "GAC", "GAA", "GAG",
+                  "GGT", "GGC", "GGA", "GGG"]
+        complete_codon_table = codon_table
+        for codon in codons:
+            try:
+                codon_table[codon]
+            except KeyError:
+                complete_codon_table.update({codon: "*"})
+        codon_series = pd.Series(complete_codon_table)[codons]
+        complete_codon_table = dict(codon_series)
+        amino_acid_arr = np.array(list(complete_codon_table.values()))
         return amino_acid_arr
 
     @staticmethod
-    def _get_genecode(ami_arr):
+    def compute_genecode(ami_arr):
         # get stop codons
         stop_codon_location__ = np.nonzero(ami_arr == "*")[0]
         number_of_stopcodon__ = len(stop_codon_location__)
@@ -84,24 +88,25 @@ class genecode_compute():
         codon_familyami__ = np.concatenate(np.nonzero(mask_ami) + ([mask_ami.size],))
         all_amifamilies__ = np.diff(codon_familyami__)
 
-       
-        return (stop_codon_location__,number_of_stopcodon__,single_codon_location__,number_of_single__,delete_location__,number_of_delete__
-                ,codon_family24__,all_24families__,number_of_eachfamily__,perm__,codon_familyami__,all_amifamilies__)
+        return (
+        stop_codon_location__, number_of_stopcodon__, single_codon_location__, number_of_single__, delete_location__,
+        number_of_delete__
+        , codon_family24__, all_24families__, number_of_eachfamily__, perm__, codon_familyami__, all_amifamilies__)
 
-    def __init__(self,amino_acid_order):
-        amino_acid_array=genecode_compute._compute_genecode(amino_acid_order)
-        data_tuple=genecode_compute._get_genecode(amino_acid_array)
-        self.stop_codon_location=data_tuple[0]
-        self.number_of_stopcodon=data_tuple[1]
-        self.single_codon_location=data_tuple[2]
-        self.number_of_single=data_tuple[3]
-        self.delete_location=data_tuple[4]
-        self.number_of_delete=data_tuple[5]
-        self.codon_family24=data_tuple[6]
-        self.all_24families=data_tuple[7]
-        self.number_of_eachfamily=data_tuple[8]
-        self.sorting=data_tuple[9]
-        self.codon_familyami=data_tuple[10]
-        self.all_amifamilies=data_tuple[11]
+    def __init__(self, genecode):
+        amino_acid_array = genecode_compute.get_genecode(genecode)
+        data_tuple = genecode_compute.compute_genecode(amino_acid_array)
+        self.stop_codon_location = data_tuple[0]
+        self.number_of_stopcodon = data_tuple[1]
+        self.single_codon_location = data_tuple[2]
+        self.number_of_single = data_tuple[3]
+        self.delete_location = data_tuple[4]
+        self.number_of_delete = data_tuple[5]
+        self.codon_family24 = data_tuple[6]
+        self.all_24families = data_tuple[7]
+        self.number_of_eachfamily = data_tuple[8]
+        self.sorting = data_tuple[9]
+        self.codon_familyami = data_tuple[10]
+        self.all_amifamilies = data_tuple[11]
 
         return
